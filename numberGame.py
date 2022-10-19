615ce56e243aff2392a58fc355c6f7cf393d7810
Nectar <74465738+ramenwithparmesancheese@users.noreply.github.com>
2022-10-19 10:46:56 -0600

    Add files via upload

---
+++ numberGame.py
@@ -0,0 +1,22 @@
+import random 
+var = -1
+
+print("The maximum number able to be generated is?")
+read = int(input()) 
+var = random.randint(0, read)
+#print(var)
+guess = None
+count = 0
+
+while guess != var:
+    print("Guess a number")
+    count += 1
+    guess = int(input())
+    if guess > var:
+        print("The answer is lower")
+    elif guess < var:
+        print("The answer is higher")
+
+print(f"You WON! It took {count} guesses")
+
+#dkmkdmak.moveMotors
